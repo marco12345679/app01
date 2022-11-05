4214a14ebf35a79beace6347f691a854b25c70d7
marco12345679 <1380628@senati.pe>
2022-11-05 18:26:04 -0500

    cusco 1

--- resources/views/cusco.blade.php
+++ resources/views/cusco.blade.php
@@ -22,27 +22,31 @@
         </style>
         </head>
          <body class="antialiased">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-         <div class="carousel-item active">
-              <img src="https://cdn.forbes.pe/2022/10/Cusco-Peru.jpg" class="d-block w-100" alt="">
-        </div>
-        <div class="carousel-item">
-          <img src="https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/07/06/000598737W.jpg" class="d-block w-100" alt="...">
-          </div>
-     <div class="carousel-item">
+         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
       <img src="https://cdn.forbes.pe/2022/10/Cusco-Peru.jpg" class="d-block w-100" alt="...">
-        </div>
     </div>
-     <button>
-         <class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <div class="carousel-item">
+      <img src="https://cms.valenciatravelcusco.com/media/images/package/city-tour-cusco_wmMCcD1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://traveler.marriott.com/es/wp-content/uploads/sites/2/2020/12/GI-930824730_MachuPicchu.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-      </div>
+</div>
     </body>
 </html>
